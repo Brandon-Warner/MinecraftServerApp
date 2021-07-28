c5b7a15f9d4ab68205f575948c1c0ceb543be275
Brandon Warner <brandonswarner5@gmail.com>
2021-07-27 17:22:17 -0700

    scrapped the DataGrid -- did not allow for one column data to be filled for the whole row

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
 import React, { useState } from 'react'
 import * as XLSX from 'xlsx'
 import axios from 'axios'
-import { DataGrid } from '@material-ui/data-grid'
+
 import { Container } from '@material-ui/core'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
 import { makeStyles } from '@material-ui/core/styles'
 import CircularProgress from '@material-ui/core/CircularProgress'
 
@@ -10,112 +17,68 @@ import Title from './components/Title'
 import Subtitle from './components/Subtitle'
 import Input from './components/Input'
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        display: 'flex',
-        '& > * + *': {
-            marginLeft: theme.spacing(2),
-        },
-    },
-}))
+// const useStyles = makeStyles(theme => ({
+//     root: {
+//         display: 'flex',
+//         '& > * + *': {
+//             marginLeft: theme.spacing(2),
+//         },
+//     },
+// }))
 
-const CircularIndeterminate = () => {
+// const CircularIndeterminate = () => {
+//     const classes = useStyles()
+
+//     return (
+//         <div className={classes.root}>
+//             <CircularProgress />
+//         </div>
+//     )
+// }
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+})
+
+const createData = (
+    hostname,
+    online,
+    ip,
+    version,
+    playersOnline,
+    playersMax,
+    blocked,
+    blockTime,
+    offlineMode
+) => {
+    return {
+        hostname,
+        online,
+        ip,
+        version,
+        playersOnline,
+        playersMax,
+        blocked,
+        blockTime,
+        offlineMode,
+    }
+}
+
+const BasicTable = () => {
     const classes = useStyles()
 
     return (
-        <div className={classes.root}>
-            <CircularProgress />
-        </div>
-    )
-}
-
-const columns = [
-    {
-        headerName: 'Hostname',
-        field: 'hostname',
-        editable: true,
-        width: 155,
-    },
-    {
-        headerName: 'Online',
-        field: 'online',
-        editable: true,
-        width: 100,
-    },
-
-    {
-        headerName: 'Ip',
-        field: 'ip',
-        editable: true,
-        width: 125,
-    },
-    {
-        headerName: 'Version',
-        field: 'version',
-        editable: true,
-        width: 125,
-    },
-
-    {
-        headerName: 'PlayersOnline',
-        field: 'playersOnline',
-        editable: true,
-        width: 150,
-    },
-    {
-        headerName: 'PlayersMax',
-        field: 'playersMax',
-        editable: true,
-        width: 150,
-    },
-    {
-        headerName: 'Blocked',
-        field: 'blocked',
-        editable: true,
-        width: 125,
-    },
-    {
-        headerName: 'BlockTime',
-        field: 'blockTime',
-        editable: true,
-        width: 125,
-    },
-    {
-        headerName: 'Offline Mode',
-        field: 'offlineMode',
-        editable: true,
-        width: 150,
-    },
-]
-
-const rows = [
-    {
-        id: 1,
-        hostname: 'Jartex',
-        online: 'yes',
-        offlineMode: 'no',
-        ip: '1111111',
-        version: '1.1',
-        playersOnline: 11,
-        playersMax: 111,
-        blocked: 'no',
-        blockTime: 'N/A',
-    },
-]
-
-const DataTable = ({ data }) => {
-    console.log('data props to DataGrid: ', data)
-    return (
-        <div>
-            <DataGrid
-                columns={columns}
-                rows={data}
-                autoHeight={true}
-                autoPageSize={true}
-                disableColumnMenu={true}
-                columnBuffer={columns.length}
-            />
-        </div>
+        <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label='Server Info'>
+                <TableHead>
+                    <TableRow>
+                        <TableCell></TableCell>
+                    </TableRow>
+                </TableHead>
+            </Table>
+        </TableContainer>
     )
 }
 
@@ -130,51 +93,27 @@ const App = () => {
     //     }
     // }
 
-    const processData = data => {
-        const generateId = () => {
-            return Math.floor(Math.random() * 1000000)
-        }
-        let newList = []
-        const list = data.split(/\r\n|\n/)
-        for (let i = 0; i < list.length; i++) {
-            let newData = {}
-            const name = list[i].toString()
-            console.log('name: ', name)
-            newData = {
-                id: generateId(),
-                hostname: name,
-                online: '',
-                ip: '',
-                version: '',
-                playersOnline: '',
-                playersMax: '',
-                blocked: '',
-                blockTime: '',
-                offlineMode: '',
-            }
-            console.log('newData: ', newData)
-            newList = [...newList, newData]
-        }
-        setData(newList)
-    }
+    // const processData = data => {
+
+    // }
 
     const handleFileUpload = e => {
         e.preventDefault()
-        const file = e.target.files[0]
-        const reader = new FileReader()
-        reader.onload = e => {
-            // PARSE DATA
-            const bstr = e.target.result
-            const workbook = XLSX.read(bstr, { type: 'binary' })
-            // GET FIRST WORKSHEET
-            const wsname = workbook.SheetNames[0]
-            const ws = workbook.Sheets[wsname]
-            // CONVERT ARRAY OF ARRAYS
-            const data = XLSX.utils.sheet_to_csv(ws, { header: 1 })
-            console.log('data from file upload: ', data)
-            processData(data)
-        }
-        reader.readAsBinaryString(file)
+        // const file = e.target.files[0]
+        // const reader = new FileReader()
+        // reader.onload = e => {
+        //     // PARSE DATA
+        //     const bstr = e.target.result
+        //     const workbook = XLSX.read(bstr, { type: 'binary' })
+        //     // GET FIRST WORKSHEET
+        //     const wsname = workbook.SheetNames[0]
+        //     const ws = workbook.Sheets[wsname]
+        //     // CONVERT ARRAY OF ARRAYS
+        //     const data = XLSX.utils.sheet_to_csv(ws, { header: 1 })
+        //     console.log('data from file upload: ', data)
+        //     processData(data)
+        // }
+        // reader.readAsBinaryString(file)
     }
     console.log('data after render: ', data)
 
@@ -184,7 +123,7 @@ const App = () => {
                 <Title />
                 <Subtitle />
                 <Input onChange={handleFileUpload} />
-                <DataTable data={data} />
+                <BasicTable />
             </div>
         </Container>
     )
