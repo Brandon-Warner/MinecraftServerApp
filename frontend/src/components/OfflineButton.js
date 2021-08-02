c5010e6bee3bbf0e6fa9d904686835d66242901e
Brandon Warner <brandonswarner5@gmail.com>
2021-08-02 15:17:31 -0700

    changed OfflineButton configuration

--- frontend/src/components/OfflineButton.js
+++ frontend/src/components/OfflineButton.js
@@ -1,41 +1,29 @@
-import React, { useState } from 'react'
+import React from 'react'
 import axios from 'axios'
 import TableCell from '@material-ui/core/TableCell'
 import { Button } from '@material-ui/core'
 
 const OfflineButton = ({ hostname }) => {
-    const [infoVisible, setInfoVisible] = useState(false)
     let offlineResponse
-
-    const hideWhenVisible = { display: infoVisible ? 'none' : '' }
-    const showWhenVisible = { display: infoVisible ? '' : 'none' }
 
     const fetchData = async hostname => {
         const data = await axios.get(
             `http://localhost:8080/api/offlineinfo/${hostname}`
         )
         const response = data.data
-        console.log('response: ', response.reason)
+        console.log('response: ', response)
         offlineResponse = response.reason
         window.alert(offlineResponse)
     }
     return (
         <TableCell>
-            <div style={hideWhenVisible}>
-                <Button
-                    variant='contained'
-                    color='primary'
-                    onClick={() => {
-                        setInfoVisible(true)
-                        fetchData(hostname)
-                    }}
-                >
-                    offline?
-                </Button>
-            </div>
-            <div style={showWhenVisible}>
-                <Button onClick={() => setInfoVisible(false)}>cancel</Button>
-            </div>
+            <Button
+                variant='contained'
+                color='primary'
+                onClick={() => fetchData(hostname)}
+            >
+                offline?
+            </Button>
         </TableCell>
     )
 }
