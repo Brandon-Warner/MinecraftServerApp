e918fd4e7176710de2d1f625d9a85b77ec67d65d
Brandon Warner <brandonswarner5@gmail.com>
2021-08-05 12:04:15 -0700

    changed date format to mm/dd/yyyy

--- frontend/src/services/servers.js
+++ frontend/src/services/servers.js
@@ -17,6 +17,23 @@ const isPlayersMax = data => {
 }
 const isBlocked = data => (data === true ? 'yes' : 'no')
 
+const formatDate = d => {
+    if (d !== null) {
+        let date = new Date(d)
+        let dd = date.getDate()
+        let mm = date.getMonth() + 1
+        let yyyy = date.getFullYear()
+        if (dd < 10) {
+            dd = '0' + dd
+        }
+        if (mm < 10) {
+            mm = '0' + mm
+        }
+        return (d = mm + '/' + dd + '/' + yyyy)
+    }
+    return 'N/A'
+}
+
 const fetchData = async name => {
     console.log('fetchData name: ', name)
     const server_data = await axios.get(`${baseUrl}/serverinfo/${name}`)
@@ -40,7 +57,7 @@ const fetchData = async name => {
         playersOnline: isPlayersOnline(serverInfo),
         playersMax: isPlayersMax(serverInfo),
         blocked: isBlocked(blockInfo.blocked),
-        blockTime: blockInfo.lastBlocked || 'N/A',
+        blockTime: formatDate(blockInfo.lastBlocked),
         offlineMode: 'N/A',
     }
     console.log('allData: ', allData)
